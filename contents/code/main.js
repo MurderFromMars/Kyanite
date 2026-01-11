050f87db0974917db90b1f8bbf84c573d47d2e86
MurderFromMars <untamedprodigy67@gmail.com>
2026-01-10 23:55:01 -0500

    Update main.js

--- contents/code/main.js
+++ contents/code/main.js
@@ -1,17 +1,4 @@
-//Kyanite - Dynamic Workspace Management for Plasma 6
-//Copyright (C) 2026 MurderFromMars
-
-//This program is free software: you can redistribute it and/or modify
-//it under the terms of the GNU General Public License version 3 as
-//published by the Free Software Foundation.
-
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//GNU General Public License for more details.
-//You should have received a copy of the GNU General Public License
-//along with this program. If not, see <https://www.gnu.org/licenses/>.
-
+// Kyanite | Smart dynamic workspace management for Plasma 6
 
 const MIN_DESKTOPS = 1;
 const LOG_LEVEL = 2;
